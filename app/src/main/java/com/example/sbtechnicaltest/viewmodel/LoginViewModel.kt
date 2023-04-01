448bb66e04975eae4b90572638362428f3da11eb
davidakinsanya <davidakinsanya00@gmail.com>
2023-04-01 13:30:15 +0100

    Writing the UI Components for the login page.

---
+++ app/src/main/java/com/example/sbtechnicaltest/viewmodel/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.sbtechnicaltest.viewmodel
+
+import androidx.navigation.NavController
+
+class LoginViewModel(private val email: String,
+                     private val password: String,
+                     private val navController: NavController) {
+
+}
